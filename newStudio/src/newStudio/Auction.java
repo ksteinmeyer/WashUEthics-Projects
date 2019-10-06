6541a05982e860294d82d2051de6a88acbcad251
Katie Steinmeyer <ksteinmeyer@wustl.edu>
2019-10-06 14:40:36 -0500

    cleaned code

--- newStudio/src/newStudio/Auction.java
+++ newStudio/src/newStudio/Auction.java
@@ -1,6 +1,5 @@
 package newStudio;
 
-//import cse131.ArgsProcessor;
 
 public class Auction {
 
@@ -14,17 +13,13 @@ public class Auction {
 
     private final int numberOfBidders = 2;
 
-    // Bidder arrays
+    //bidder arrays
     private String[] bidderNames = new String[numberOfBidders];
     private int[] bidderMaxBids = new int[numberOfBidders];
     private int[] bidderCurrentBids = new int[numberOfBidders];
 
 
-//    private String[] bidName; //using parallel array to store information about the bid's name, amount, and max bid
-//    private int[] bidAmount;
-//    private int[] bidMax;
 
-  //  private int size; //size of arrays
     private String winner;
 
     public static void main(String[] args) {
@@ -33,8 +28,7 @@ public class Auction {
         Bidder playerTwo= new Bidder("Player 2", 11 , 1);
 
         Auction auction = new Auction("auction",2, 1, playerOne, playerTwo);
-       // auction.addBid(a);
-       // auction.addBid(b);
+
     }
 
     public Auction(String name, int incrementAmount, int startingValue, Bidder firstBidder, Bidder secondBidder){ //constructor creates object and initializes variables inside
@@ -74,31 +68,7 @@ public class Auction {
             currentWinner();
         }
     }
-//    public boolean addBid(Bidder a){ //method for adding bid, returns true if bid was successfully added to auction,
-//        // returns false if bid did not qualify for auction
-//        if(bidQualify(a)) {
-//
-////            bidAmount[size] = a.getBidAmount(); //adds each piece of information to parallel array
-////            bidName[size] = a.getName();
-////            bidMax[size] = a.getMaxBid();
-////            size++; //increases size of arrays
-//            currentWinner(); //recalculates the current winner using new information
-//            return true;
-//        }
-//        return false;
-//    }
-//    public int getBidIndex(String a){ //need to make sure user can withdraw bid
-//        int index = 0;
-//        for(int i = 0; i < size; i++){
-//            if(bidName[i] == a){
-//                index = i;
-//            }
-//        }
-//        return index;
-//    }
     private void currentWinner(){ //calculates current winner of auction
-
-       // for(int i = 0; i < bidAmount.length; i++){ //finds the max bid amound and saves index of that bid
             if(bidderCurrentBids[0] > bidderCurrentBids[1]){
                 pushToMax(bidderCurrentBids[1]);
             } else{
@@ -113,7 +83,7 @@ public class Auction {
             System.out.println("Winner of " + name + " is " + bidderNames[0]);
         }else {
             currentWinner();
-        }//sets new winner to the name of the bid at highest amount index
+        }
     }
     private int pushToMax(int i){
         if(bidderCurrentBids[i] + incrementAmount <= bidderMaxBids[i]){
@@ -123,35 +93,5 @@ public class Auction {
         }
         return bidderCurrentBids[i];
     }
-
-//    private void bidRaise(int []bidAmount, int []bidMax){
-//        for(int i = 0; i < size; i++){
-//            if(bidAmount[i] > bidMax[i]){
-//                delete(i);
-//            }
-//            if((bidName[i] != this.winner) && (bidAmount[i] < bidMax[i])){
-//                bidAmount[i] = bidAmount[i] + incrementAmount;
-//                currentWinner();
-//            }
-//        }
-//    }
-
-//    private void delete(int x){
-//        String toPrint = bidName[x];
-//        bidName[x] = null;
-//        bidAmount[x] = 0;
-//        bidMax[x] = 0;
-//        for(int i = x; i < size; i++){
-//            bidName[i] = bidName[i+1];
-//            bidMax[i] = bidMax[i+1];
-//            bidAmount[i] = bidAmount[i+1];
-//        }
-//        bidName[size] = null;
-//        bidAmount[size] = 0;
-//        bidMax[size] = 0;
-//        size--;
-//        System.out.println("Bid " + toPrint + " is no longer in auction: " + this.name);
-//    }
-
 
 }
